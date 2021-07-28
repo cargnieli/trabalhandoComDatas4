1ec202ca0095097b84f0b0fc2eba47f8991685f0
Walter Donizety Cargnieli <cargnieli@yahoo.com>
2021-07-28 13:20:36 -0300

    Trabalhando com datas

---
+++ src/com/local/TrabalhandoComDatas.java
@@ -0,0 +1,18 @@
+package com.local;
+
+import java.util.Date;
+
+public class TrabalhandoComDatas {
+
+    public static void main(String[] args) {
+
+        Date novaData = new Date();
+        System.out.println(novaData);
+
+
+
+
+
+
+    }
+}
